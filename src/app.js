c21c3bb16077fc166fb147a8ee30bfb84128dd82
Pavan <reddypavankumar709@gmail.com>
2025-12-24 13:36:03 +0530

    These the fixing the frontend issues

--- src/app.js
+++ src/app.js
@@ -6,28 +6,41 @@ const authRoutes = require("./routes/auth.routes");
 const foodRoutes = require("./routes/food.routes");
 const foodPartnerRoutes = require("./routes/food-partner.routes");
 
-const app = express(); // 
-const port = process.env.PORT || 3000;
+const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://694b98ac91fdb80d947bc7e1--glittering-baklava-71eae1.netlify.app"
+];
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5174",
-      "https://food-reels-seven.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (Postman, mobile apps)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed from this origin: " + origin));
+    }
+  },
+  credentials: true
+}));
+
+// Enable preflight requests for cookies
+app.options("*", cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 
 
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.get("/", (req, res) => {
   res.send("FoodReels API is running 🚀");
 });
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/food", foodRoutes);
